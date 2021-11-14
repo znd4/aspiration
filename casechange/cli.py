366d3287913a4f4ab6dcb01ce261f36be033be2c
Zane Dufour <zane@znd4.me>
2021-11-14 10:53:51 -0500

    Add mkdocs-click

--- casechange/cli.py
+++ casechange/cli.py
@@ -1,28 +1,38 @@
-import string
+import string as string_
 
 import typer
 
-app = typer.Typer()
+app = typer.Typer(add_completion=False)
 
-ALPHANUMERICS = {*string.ascii_letters, *string.digits}
+
+# rename so that we can use the argument name string in main
+ALPHANUMERICS = {*string_.ascii_letters, *string_.digits}
 
 
 @app.command()
-def main(s: str, n: int):
-    """Augment `s` so that every `n`th alphanumeric character is capitalized.
+def main(sequence: str, n: int):
+    """Augment `sequence` so that every `n`th alphanumeric character is capitalized.
 
+    ```sh
     $ casechange Abc*-2fr 3
     abC*-2fR
+
     $ casechange r1.abB 2
     r1.aBb
+    ```
     """
 
     alphanumeric_index = 0
-    for i, char in enumerate(s):
+    for i, char in enumerate(sequence):
         if char in ALPHANUMERICS:
             alphanumeric_index = (alphanumeric_index + 1) % n
             if alphanumeric_index:
-                s = s[:i] + s[i].lower() + s[i + 1 :]
+                sequence = sequence[:i] + sequence[i].lower() + sequence[i + 1 :]
             else:
-                s = s[:i] + s[i].upper() + s[i + 1 :]
-    typer.echo(s)
+                sequence = sequence[:i] + sequence[i].upper() + sequence[i + 1 :]
+    typer.echo(sequence)
+
+
+# This is added for documentation purposes (the mkdocs-click plugin needs access to the
+# underlying click app)
+click_app = typer.main.get_command(app)
