e1a6bd342e2bb5a363a97e886bda5965361a27f6
Zane Dufour <zane@znd4.me>
2021-11-15 19:14:02 -0500

    fix numpy

--- casechange/methods.py
+++ casechange/methods.py
@@ -24,6 +24,21 @@ def numpy(s: str, n: int) -> str:
     Find the indices of the alphanumeric characters, apply `lower`
     and `upper` to those indices according to `n`, and then consolidate
     the array into a single string.
+
+    I haven't profiled this yet, but I expect this to be much faster than naive.
+
+    ### Scaling
+
+    This has a constant number of $O(N)$ numpy operations, so it's $O(N)$
+
+    ### Numpy is compiled
+
+    There isn't a python for loop in sight (numpy is written in C)
+
+    ### Numpy is vectorized
+
+    Because this uses numpy, many of the operations involved here are performed in
+    parallel.
     """
     # importing numpy inside a function isn't great, because the import
     # takes a really long time. The import process only needs to happen once,
@@ -32,12 +47,16 @@ def numpy(s: str, n: int) -> str:
     # dependency
     import numpy as np
 
-    s_arr = np.array(list(s))
+    s_arr = np.array(list(s), dtype=str)
 
     alnums = np.where(np.char.isalnum(s_arr))[0]
-    is_not_nth = (alnums + 1) % n
+
+    is_not_nth = (np.arange(alnums.shape[0]) + 1) % n
+
     lowers = alnums[np.where(is_not_nth)]
     uppers = alnums[np.where(np.logical_not(is_not_nth))]
-    s_arr[uppers]
 
-    raise NotImplementedError()
+    s_arr[uppers] = np.char.upper(s_arr[uppers])
+    s_arr[lowers] = np.char.lower(s_arr[lowers])
+
+    return "".join(s_arr)
